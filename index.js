f4c0d457b067a5fea4ee1849b2e75df3ffb1ed50
binaf <ousmanehaidara26@gmail.com>
2022-03-07 21:15:25 +0100

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// console.log([1,2,3,4,5].at(3))
+
+const dectectDeviceType = () =>
+  /Andriod|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+    ? 'mobile'
+    : 'desktop';
+// console.log(dectectDeviceType);
